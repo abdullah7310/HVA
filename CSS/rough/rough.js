ac3adf2b821fdf0ad0c091973a3a07ce30e924f8
Abdullah Ahmad <abdul7310ahmad@gmail.com>
2024-08-01 11:35:32 +0530

    added new css files

---
+++ CSS/rough/rough.js
@@ -0,0 +1,22 @@
+// function sum(x,y){
+//     s = x+y
+//     return s;
+// }
+// console.log(sum(5,9));
+
+// function mul(x,y){
+//     multi = x*y;
+//     return multi;
+// }
+// console.log(mul(5,4));
+// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&  ARROW FUNCTION  &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&7
+// const multiply = (x,y) => {
+//     console.log(x*y);
+// }
+// console.log(multiply(4,6));
+
+// ************************************  FOR IN LOOP  ******************************************
+let arr = ["delhi","mumbai","pune","bihar"]
+for (let x in arr){
+    console.log(arr[x],x);
+}
