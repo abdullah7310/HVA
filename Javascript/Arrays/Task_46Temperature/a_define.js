d59e417baa0365133387aa76f7e7d6c9972bb03e
Abdullah Ahmad <abdul7310ahmad@gmail.com>
2024-08-13 11:19:53 +0530

    added new file

---
+++ Javascript/Arrays/Task_46Temperature/a_define.js
@@ -0,0 +1,11 @@
+let temperature = [-3,14,22,5,-10]
+// temperature.forEach(function (temp){
+//     console.log(temp);
+// })
+
+// ************Arrow Function Temperature ***************
+
+temperature.forEach((temp)=>{
+    console.log((temp*9/5)+32)
+})
+
